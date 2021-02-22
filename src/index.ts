de3179161bf68a667dbf0417abc5348239e52343
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-23 01:00:45 +0900

    Init project

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import "./style.css";
+
+(async () => {
+  alert("hello!");
+})();
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+const common = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  entry: "./src",
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json", ".mjs", ".wasm"],
+  },
+};
+
+const dev = {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
+};
+
+const prod = {
+  mode: "production",
+};
+
+module.exports = function () {
+  if (process.env.NODE_ENV === "production") {
+    return { ...common, ...prod };
+  } else {
+    return { ...common, ...dev };
+  }
+};
