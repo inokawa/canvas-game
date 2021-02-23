cf5da0ad6cf2fcf75d5556edf42ddfe81cccd3e4
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-23 11:10:42 +0900

    Refactor

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
-import { Canvas2DUtility } from "./canvas";
+import * as util from "./canvas";
 import { loadImage } from "./utils";
 import viperImage from "./assets/images/viper.png";
 
 export const init = async () => {
-  const screen = document.querySelector("#screen") as HTMLCanvasElement;
+  const canvas = document.querySelector("#screen") as HTMLCanvasElement;
   const image = await loadImage(viperImage);
 
   const CANVAS_WIDTH = 640;
   const CANVAS_HEIGHT = 480;
 
-  const util = new Canvas2DUtility(screen, CANVAS_WIDTH, CANVAS_HEIGHT);
+  canvas.width = CANVAS_WIDTH;
+  canvas.height = CANVAS_HEIGHT;
+  const ctx = canvas.getContext("2d")!;
 
   let isComing = true;
   let x = CANVAS_WIDTH / 2;
@@ -39,8 +41,8 @@ export const init = async () => {
   render();
 
   function render() {
-    util.context.globalAlpha = 1.0;
-    util.drawRect(0, 0, util.canvas.width, util.canvas.height, "#eeeeee");
+    ctx.globalAlpha = 1.0;
+    util.drawRect(ctx, 0, 0, canvas.width, canvas.height, "#eeeeee");
     const currentTime = (Date.now() - startTime) / 1000;
 
     if (isComing) {
@@ -52,11 +54,11 @@ export const init = async () => {
         y = CANVAS_HEIGHT - 100;
       }
       if (justTime % 100 < 50) {
-        util.context.globalAlpha = 0.5;
+        ctx.globalAlpha = 0.5;
       }
     }
 
-    util.context.drawImage(image, x, y);
+    ctx.drawImage(image, x, y);
     requestAnimationFrame(render);
   }
 };
--- src/canvas.ts
+++ src/canvas.ts
@@ -1,18 +1,13 @@
-export class Canvas2DUtility {
-  canvas: HTMLCanvasElement;
-  context: CanvasRenderingContext2D;
-
-  constructor(canvas: HTMLCanvasElement, width: number, height: number) {
-    this.canvas = canvas;
-    this.canvas.width = width;
-    this.canvas.height = height;
-    this.context = canvas.getContext("2d")!;
+export const drawRect = (
+  context: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  color: string
+) => {
+  if (color != null) {
+    context.fillStyle = color;
   }
-
-  drawRect(x: number, y: number, width: number, height: number, color: string) {
-    if (color != null) {
-      this.context.fillStyle = color;
-    }
-    this.context.fillRect(x, y, width, height);
-  }
-}
+  context.fillRect(x, y, width, height);
+};
