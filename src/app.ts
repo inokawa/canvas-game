6eff7e9f180f48dad9433078ef89e2586d281d6d
inokawa <48897392+inokawa@users.noreply.github.com>
2021-02-23 10:52:06 +0900

    Handle keydown event

--- src/app.ts
+++ src/app.ts
@@ -10,17 +10,36 @@ export const init = async () => {
   const CANVAS_HEIGHT = 480;
 
   const util = new Canvas2DUtility(screen, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  let x = CANVAS_WIDTH / 2;
+  let y = CANVAS_HEIGHT / 2;
+
+  window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+      case "ArrowLeft":
+        x -= 10;
+        break;
+      case "ArrowRight":
+        x += 10;
+        break;
+      case "ArrowUp":
+        y -= 10;
+        break;
+      case "ArrowDown":
+        y += 10;
+        break;
+      default:
+        break;
+    }
+  });
+
   const startTime = Date.now();
   render();
 
   function render() {
     util.drawRect(0, 0, util.canvas.width, util.canvas.height, "#eeeeee");
 
-    const currentTime = (Date.now() - startTime) / 1000;
-    const s = Math.sin(currentTime);
-    const x = s * 100.0;
-
-    util.context.drawImage(image, CANVAS_WIDTH / 2 + x, CANVAS_HEIGHT / 2);
+    util.context.drawImage(image, x, y);
     requestAnimationFrame(render);
   }
 };
